5f386eeaf5c43c9c538f984a92a1f650e0f9e2b6
ArthurEstevaum <arthurestevao631@gmail.com>
2025-10-03 01:49:05 -0300

    Bootstrap project with spring initializr

---
+++ src/main/java/com/stefanini/agile_agent/AgileAgentApplication.java
@@ -0,0 +1,13 @@
+package com.stefanini.agile_agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgileAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgileAgentApplication.class, args);
+	}
+
+}
